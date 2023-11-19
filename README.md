fdf2312bf9b6c83a8e441496df718970119929c3
Sooraj Gupta <soorajgupta00@gmail.com>
2023-11-20 02:24:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# feedbackApp-withMockJSONServer
+This one Uses a Mock JSON Server to Serve &amp; Save the data 
---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,94 @@
+import { createContext, useState, useEffect } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch Feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
+
+  // Add feedback
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch(`/feedback`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
+  };
+
+  // Delete Feedback
+  const deleteFeedback = async (id) => {
+    if (window.confirm("Are you sure you want to delete this feedback?")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" });
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  // Update Feedback Item
+  const updateFeedback = async (id, updatedItemId) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedItemId),
+    });
+
+    const data = await response.json();
+
+    setFeedback(feedback.map((item) => (item.id === id ? data : item)));
+
+    setFeedbackEdit({
+      item: {},
+      edit: false,
+    });
+  };
+
+  //Set Item to be Updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit,
+        updateFeedback,
+        isLoading,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
